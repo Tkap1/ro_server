84bf399fd54b83526c05330dd65cffcf4c38ca24
Tkap1 <javijpa@hotmail.com>
2019-11-30 10:51:39 +0100

    nightmare arclouze now drops its card

--- src/map/battle.c
+++ src/map/battle.c
@@ -4660,7 +4660,7 @@ static struct Damage battle_calc_weapon_attack(struct block_list *src, struct bl
 	// 	skill_id == KN_AUTOCOUNTER ||
 	// 	skill_id == SN_SHARPSHOOTING || skill_id == MA_SHARPSHOOTING ||
 	// 	skill_id == NJ_KIRIKAGE))
-	if( !flag.cri && sstatus->cri && // Tkap: Messing with crit
+	if( !flag.cri && sstatus->cri && // Tkap: Double attack can crit
 		(!skill_id ||
 		skill_id == KN_AUTOCOUNTER ||
 		skill_id == SN_SHARPSHOOTING || skill_id == MA_SHARPSHOOTING ||
